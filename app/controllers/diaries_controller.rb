48b54afc284003e394bcf7921a4a48cd1e6c4712
me <mie501.648@gmail.com>
2025-04-26 16:12:53 +0900

    日記編集機能を追加

--- app/controllers/diaries_controller.rb
+++ app/controllers/diaries_controller.rb
@@ -1,5 +1,6 @@
 class DiariesController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_diary, only: [:show, :edit, :update, :destroy]
 
   def new
     @diary = Diary.new
@@ -22,10 +23,34 @@ class DiariesController < ApplicationController
   end
 
   def show
-    @diary = Diary.find(params[:id])
+  end
+
+  def edit
+  end
+
+  def update
+    if params[:diary][:remove_image] == '1'
+      @diary.image.purge
+    end
+
+    if @diary.update(diary_params.except(:remove_image))
+      # redirect_to diaries_path(@diary)
+      redirect_to diaries_path
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @diary.destroy
+    redirect_to diaries_path
   end
 
   private
+
+  def set_diary
+    @diary = Diary.find(params[:id])
+  end
 
   def diary_params
     params.require(:diary).permit(:title, :image, :text)
